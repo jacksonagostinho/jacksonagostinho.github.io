21b2b65eacef2a1c5ce55c45ab15ee6b1c8ddcfa
Jackson Willian <jacksonwilliansilvaagostinho@gmail.com>
2021-09-07 09:19:04 -0300

    feat: adiciona base visual

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalculatorComponent } from './components/template/calculator/calculator.component';
+import { DisplayComponent } from './components/template/display/display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatorComponent,
+    DisplayComponent
   ],
   imports: [
     BrowserModule,
